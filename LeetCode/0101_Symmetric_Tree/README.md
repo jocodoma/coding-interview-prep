969a3fba41014bd93eb913d4d217ce830ea5e81e
Joseph Chen <josephchen3@gmail.com>
2019-10-24 15:49:59 -0700

    Add 101 Symmetric Tree

---
+++ LeetCode/0101_Symmetric_Tree/README.md
@@ -0,0 +1,2 @@
+# Problem 101: [Symmetric Tree](https://leetcode.com/problems/symmetric-tree/)
+![Difficulty](https://img.shields.io/badge/Difficulty-Easy-brightgreen.svg) ![Language](https://img.shields.io/badge/Language-C++%2011,%20Python,%20JavaScript-yellow) [![License](https://img.shields.io/badge/License-MIT-blue.svg)](../LICENSE)
---
+++ LeetCode/0101_Symmetric_Tree/symmetric_tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        // return isSymmetric_Recursive(root);
+        return isSymmetric_Iterative(root);
+    }
+
+    bool isSymmetric_Recursive(TreeNode* root){
+        return isMirror(root, root);
+    }
+
+    bool isMirror(TreeNode* leftSubTree, TreeNode* rightSubTree){
+        if(!leftSubTree && !rightSubTree) return true;
+        if(!leftSubTree || !rightSubTree) return false;
+        if(leftSubTree->val != rightSubTree->val) return false;
+        return isMirror(leftSubTree->left, rightSubTree->right) && isMirror(leftSubTree->right, rightSubTree->left);
+    }
+
+    bool isSymmetric_Iterative(TreeNode* root){
+        stack<TreeNode*> nodeStack;
+        nodeStack.push(root);
+        nodeStack.push(root);
+
+        while(!nodeStack.empty()){
+            TreeNode *leftNode = nodeStack.top(); nodeStack.pop();
+            TreeNode *rightNode = nodeStack.top(); nodeStack.pop();
+            if(!leftNode && !rightNode) continue;
+            if(!leftNode || !rightNode) return false;
+            if(leftNode->val != rightNode->val) return false;
+            nodeStack.push(leftNode->left);
+            nodeStack.push(rightNode->right);
+            nodeStack.push(leftNode->right);
+            nodeStack.push(rightNode->left);
+        }
+        return true;
+    }
+};
