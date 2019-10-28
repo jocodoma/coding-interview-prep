b09b4509017235d594e40f9a11993d2d641bbe9f
Joseph Chen <josephchen3@gmail.com>
2019-10-28 16:06:54 -0700

    Add 160 Intersection of Two Linked Lists

---
+++ LeetCode/0160_Intersection_of_Two_Linked_Lists/README.md
@@ -0,0 +1,2 @@
+# Problem 160: [Intersection of Two Linked Lists](https://leetcode.com/problems/intersection-of-two-linked-lists/)
+![Difficulty](https://img.shields.io/badge/Difficulty-Easy-brightgreen.svg) ![Language](https://img.shields.io/badge/Language-C++%2011-yellow) [![License](https://img.shields.io/badge/License-MIT-blue.svg)](../LICENSE)
---
+++ LeetCode/0160_Intersection_of_Two_Linked_Lists/intersection_of_two_linked_lists.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // return bruteForce(headA, headB);
+        // return lookupHashTable(headA, headB);
+        return twoPointersOneList(headA, headB);
+    }
+
+    // time complexity: O(nm), space complexity: O(1)
+    ListNode *bruteForce(ListNode *headA, ListNode *headB){
+        ListNode *currA, *currB;
+        currA = headA;
+        while(currA){
+            currB = headB;
+            while(currB){
+                if(currA == currB) return currA;
+                currB = currB->next;
+            }
+            currA = currA->next;
+        }
+        return NULL;
+    }
+
+    // time complexity: O(n+m), space complexity: O(m) or O(m)
+    ListNode *lookupHashTable(ListNode *headA, ListNode *headB){
+        ListNode *currA = headA;
+        ListNode *currB = headB;
+        std::unordered_set<ListNode*> lookupTable;
+        // build a map
+        while(currA){
+            lookupTable.insert(currA);
+            currA = currA->next;
+        }
+        // lookup a map
+        while(currB){
+            if(lookupTable.count(currB)) return currB;
+            currB = currB->next;
+        }
+        return NULL;
+    }
+
+    // combine two lists to create a new list (length = n + m)
+    // time complexity: O(n+m), space complexity: O(1)
+    ListNode *twoPointersOneList(ListNode *headA, ListNode *headB){
+        ListNode *currA = headA;
+        ListNode *currB = headB;
+        while(currA != currB){
+            currA = currA ? currA->next : headB;
+            currB = currB ? currB->next : headA;
+        }
+        return currA;
+    }
+};
