e18cd12eb692b1971b23affe1c8698998ad9c6f3
Joseph Chen <josephchen3@gmail.com>
2019-10-29 15:49:21 -0700

    Add 169 Majority Element

---
+++ LeetCode/0169_Majority_Element/README.md
@@ -0,0 +1,2 @@
+# Problem 169: [Majority Element](https://leetcode.com/problems/majority-element/)
+![Difficulty](https://img.shields.io/badge/Difficulty-Easy-brightgreen.svg) ![Language](https://img.shields.io/badge/Language-C++%2011-yellow) [![License](https://img.shields.io/badge/License-MIT-blue.svg)](../LICENSE)
---
+++ LeetCode/0169_Majority_Element/majority_element.cpp
@@ -0,0 +1,41 @@
+// more solutions on: https://leetcode.com/problems/majority-element/discuss/51612/C%2B%2B-6-Solutions
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // return bruteForce(nums);
+        // return hashTable(nums);
+        return sorting(nums);
+    }
+
+    // time complexity: O(n^2), space complexity: O(1)
+    int bruteForce(vector<int>& nums){
+        int majorityCount = nums.size()/2;
+        for(int i = 0; i < nums.size(); i++){
+            int count = 0;
+            for(int j = 0; j < nums.size(); j++)
+                if(nums[i] == nums[j]) count++;
+
+            if(count > majorityCount) return nums[i];
+        }
+        return -1;  // no majority element found
+    }
+
+    // time complexity: O(n), space complexity: O(n)
+    int hashTable(vector<int>& nums){
+        int majorityCount = nums.size()/2;
+        std::unordered_map<int, int> map;
+        for(int i = 0; i < nums.size(); i++){
+            if(map.count(nums[i])) map[nums[i]]++;
+            else map[nums[i]] = 1;
+            if(map[nums[i]] > majorityCount) return nums[i];
+        }
+        return -1;  // no majority element found
+    }
+
+    // time complexity: O(nlogn), space complexity: O(1) - sorted in place
+    int sorting(vector<int>& nums){
+        std::nth_element(nums.begin(), nums.begin() + nums.size()/2, nums.end());
+        return nums[nums.size()/2];
+    }
+};
