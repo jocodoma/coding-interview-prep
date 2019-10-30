1ba487a2c0fabf5cd427678b4aa1885d7fa3a2c2
Joseph Chen <josephchen3@gmail.com>
2019-10-29 17:50:03 -0700

    Add 198 House Robber

---
+++ LeetCode/0198_House_Robber/README.md
@@ -0,0 +1,2 @@
+# Problem 198: [House Robber](https://leetcode.com/problems/house-robber/)
+![Difficulty](https://img.shields.io/badge/Difficulty-Easy-brightgreen.svg) ![Language](https://img.shields.io/badge/Language-C++%2011-yellow) [![License](https://img.shields.io/badge/License-MIT-blue.svg)](../LICENSE)
---
+++ LeetCode/0198_House_Robber/house_robber.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        // return maxArray(nums);
+        return maxVars(nums);
+    }
+
+    // time complexity: O(n), space complexity: O(n)
+    int maxArray(vector<int>& nums){
+        int length = nums.size();
+        if(length == 0) return 0;
+        if(length == 1) return nums[0];
+        if(length == 2) return std::max(nums[0], nums[1]);
+
+        int maxArray[length];
+        maxArray[0] = nums[0];
+        maxArray[1] = std::max(maxArray[0], nums[1]);
+        for(int i = 2; i < length; i++)
+            maxArray[i] = std::max(maxArray[i-1], nums[i] + maxArray[i-2]);
+
+        return maxArray[length-1];
+    }
+
+    // time complexity: O(n), space complexity: O(1)
+    int maxVars(vector<int>& nums){
+        if(nums.empty()) return 0;
+        int length = nums.size();
+        int preNotAdjMax = 0, preAdjMax = 0, currMax = 0;
+        for(int i = 0; i < length; i++){
+            currMax = std::max(preAdjMax, nums[i] + preNotAdjMax);
+            preNotAdjMax = preAdjMax;
+            preAdjMax = currMax;
+        }
+        return currMax;
+    }
+};
