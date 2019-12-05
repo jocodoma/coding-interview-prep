3f83dc7f4165049594255bb6ea3af4a24c54a881
Joseph Chen <jocodoma@gmail.com>
2019-12-05 15:10:54 -0800

    Add 62 Unique Paths

---
+++ LeetCode/0062_Unique_Paths/README.md
@@ -0,0 +1,2 @@
+# Problem 62: [Unique Paths](https://leetcode.com/problems/unique-paths/)
+![Difficulty](https://img.shields.io/badge/Difficulty-Medium-orange.svg) ![Language](https://img.shields.io/badge/Language-C++%2011-yellow) [![License](https://img.shields.io/badge/License-MIT-blue.svg)](../LICENSE)
---
+++ LeetCode/0062_Unique_Paths/unique_paths.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        return dpArray(m, n);
+        // return dpVector(m, n);
+        // return dp2(m, n);
+        // return dp3(m, n);
+    }
+
+    // time complexity: O(mn), space complexity: O(mn)
+    int dpArray(int m, int n){
+        if(m < 1 || n < 1) return 0;
+
+        // initialization
+        int dp[m][n] = {};  // for c => int dp[m][n] = {0};
+        for(int i = 0; i < m; i++) dp[i][0] = 1;
+        for(int j = 0; j < n; j++) dp[0][j] = 1;
+
+        for(int i = 1; i < m; i++)
+            for(int j = 1; j < n; j++)
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+
+        return dp[m-1][n-1];
+    }
+
+    // time complexity: O(mn), space complexity: O(mn)
+    int dpVector(int m, int n){
+        if(m < 1 || n < 1) return 0;
+
+        // create a vector containing m vectors of size n with value 1
+        std::vector<std::vector<int>> dp(m, std::vector<int>(n, 1));
+
+        for(int i = 1; i < m; i++)
+            for(int j = 1; j < n; j++)
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+
+        return dp[m-1][n-1];
+    }
+
+    // Reference: https://leetcode.com/problems/unique-paths/discuss/22954/C%2B%2B-DP
+    // The above solution runs in O(mn) time and costs O(mn) space.
+    // However, you may have noticed that each time when we update dp[i][j],
+    // we only need dp[i - 1][j] (at the previous row) and dp[i][j - 1] (at the current row).
+    // So we can reduce the memory usage to just two rows (O(n)).
+
+    // time complexity: O(mn), space complexity: O(2n) = O(n)
+    int dp2(int m, int n){
+        if(m < 1 || n < 1) return 0;
+
+        std::vector<int> pre(n, 1), cur(n, 1);
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++)
+                cur[j] = pre[j] + cur[j-1];
+
+            std::swap(pre, cur);
+        }
+
+        return pre[n-1];
+    }
+
+    // Reference: https://leetcode.com/problems/unique-paths/discuss/22954/C%2B%2B-DP
+    // Further inspecting the above code, pre[j] is just the cur[j] before the update.
+    // So we can further reduce the memory usage to one row.
+
+    // time complexity: O(mn), space complexity: O(n)
+    int dp3(int m, int n){
+        if(m < 1 || n < 1) return 0;
+
+        std::vector<int> cur(n, 1);
+        for(int i = 1; i < m; i++)
+            for(int j = 1; j < n; j++)
+                cur[j] += cur[j-1];
+
+        return cur[n-1];
+    }
+};
