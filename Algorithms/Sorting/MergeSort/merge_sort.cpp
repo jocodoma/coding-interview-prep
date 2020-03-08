e670d3418db80c1a618571c5893fb7a46c8fa656
Joseph Chen <jocodoma@gmail.com>
2020-03-07 16:52:53 -0800

    Add the example code of merge sort

---
+++ Algorithms/Sorting/MergeSort/merge_sort.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+
+// Utility function to find minimum of two integers
+// int min(int x, int y){ return (x < y) ? x : y; }
+
+// merges two subarrys of vec[]
+// first subarray is vec[l..m]
+// second subarray is vec[m+1..r]
+void merge(std::vector<int>& vec, int l, int m, int r){
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    std::vector<int> L(n1), R(n2);  // temp arrays
+
+    // copy data to temp arrays
+    for(int i = 0; i < n1; i++)
+        L[i] = vec[l+i];
+
+    for(int i = 0; i < n1; i++)
+        R[i] = vec[m+1+i];
+
+    // merge the temp arrays back into vec[l..r]
+    int i = 0, j = 0, k = l;
+    while(i < n1 && j < n2){
+        if(L[i] <= R[j])
+            vec[k++] = L[i++];
+        else
+            vec[k++] = R[j++];
+    }
+
+    // copy the remaining elements of L[] or R[], if there are any
+    while(i < n1)
+        vec[k++] = L[i++];
+
+    while(j < n2)
+        vec[k++] = R[j++];
+}
+
+void mergeSortIterative(std::vector<int>& vec){
+    int n = vec.size();
+    // divide the array into blocks of size bs
+    // bs = 1, 2, 4, 8, 16, ...
+    for(int bs = 1; bs < n; bs *= 2){  // size of block
+        // bs = 1, i = 0, 2, 4, 6, 8, ...
+        // bs = 2, i = 0, 4, 8, ...
+        // bs = 4, i = 0, 8, ...
+        for(int i = 0; i < n; i += bs*2){  // number of block
+            // find ending point of left subarray
+            // (bs+1) is starting point of right
+            int l = i;
+            int m = i + bs - 1;
+            int r = std::min(i + bs*2 - 1, n-1);  // (n-1) will be chosen if remain elements are fewer than (m*2)
+            merge(vec, l, m, r);
+        }
+    }
+}
+
+void mergeSortRecursive(std::vector<int>& vec, int l, int r){
+    if(l < r){
+        // same as (l+r)/2, but avoids overflow for large l and h
+        int m = l + (r - l) / 2;
+
+        // sort first and second halves, then merge
+        mergeSortRecursive(vec, l, m);
+        mergeSortRecursive(vec, m+1, r);
+        merge(vec, l, m, r);
+    }
+}
+
+void printVector(std::vector<int> vec){
+    for(const auto& i : vec)
+        std::cout << i << " ";
+    std::cout << std::endl;
+}
+
+int main(){
+    std::vector<int> input= {12, 11, 13, 5, 6, 7};
+
+    std::cout << "Given array is \n";
+    printVector(input);
+
+    mergeSortRecursive(input, 0, input.size()-1);
+    // mergeSortIterative(input);
+
+    std::cout << "\nSorted array is \n";
+    printVector(input);
+
+    return 0;
+}
---
+++ Algorithms/Sorting/MergeSort/merge_sort_linked_list_recursive.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+
+// linked list node
+struct Node {
+    int data;
+    Node *next;
+    Node(int x) : data(x), next(NULL) {}
+};
+
+Node* merge(Node* a, Node* b){
+    Node dummy(0);  // to avoid potential memory leak
+    Node *tail = &dummy;
+
+    // base case
+    if(a == NULL)
+        return b;
+    if(b == NULL)
+        return a;
+
+    // pick either a or b
+    if(a->data <= b->data){
+        tail->next = a;
+        tail = tail->next;
+        tail->next = merge(a->next, b);
+    }
+    else{
+        tail->next = b;
+        tail = tail->next;
+        tail->next = merge(a, b->next);
+    }
+    return dummy.next;
+}
+
+void frontBackSplit(Node* currHead, Node*& frontHead, Node*& backHead){
+    Node *slow, *fast;
+    slow = currHead;
+    fast = currHead->next;
+
+    while(fast != NULL){
+        fast = fast->next;
+        if(fast != NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+
+    // slow should be before the midpoint in the list
+    frontHead = currHead;
+    backHead = slow->next;
+
+    slow->next = NULL;
+}
+
+void mergeSort(Node*& headNode){
+    Node *head = headNode;
+    Node *a, *b;
+
+    // base case
+    if(head == NULL || head->next == NULL)
+        return;
+    
+    // split head into two halves, a and b
+    frontBackSplit(head, a, b);
+
+    // recursively sort the sublists
+    mergeSort(a);
+    mergeSort(b);
+
+    // sort and merge the two list
+    headNode = merge(a, b);
+}
+
+void printList(Node* node)
+{
+    while(node != NULL){
+        std::cout << node->data << " ";
+        node = node->next;
+    }
+    std::cout << std::endl;
+}
+  
+// function to insert a node at the beginging of the linked list
+void push(Node** headNode, int newData){
+    Node *newNode = new Node(newData);
+    newNode->next = *headNode;
+    *headNode = newNode;  // insert the new node to the head of list
+}
+
+void release(Node*& headNode){
+    Node *currNode = headNode;
+    while(currNode != NULL){
+        Node *nextNode = currNode->next;
+        delete currNode;
+        currNode = nextNode;
+    }
+    headNode = NULL;
+}
+
+int main(){
+    Node* a = NULL;
+
+    // create a unsorted linked lists to test the functions
+    // created lists shall be a: 2->3->20->5->10->15
+    push(&a, 15);
+    push(&a, 10);
+    push(&a, 5);
+    push(&a, 20);
+    push(&a, 3);
+    push(&a, 2);
+
+    std::cout << "Given array is \n";
+    printList(a);
+
+    // sort the above created Linked List
+    mergeSort(a);
+  
+    std::cout << "\nSorted Linked List is: \n";
+    printList(a);
+  
+    release(a);
+
+    return 0;
+}
