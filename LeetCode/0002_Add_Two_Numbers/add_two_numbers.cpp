4b946bec857bcdb5341d7b92ece7dc441f04876c
Joseph Chen <jocodoma@gmail.com>
2020-03-11 17:10:52 -0700

    [LeetCode] Add recursive code for 0002

--- LeetCode/0002_Add_Two_Numbers/add_two_numbers.cpp
+++ LeetCode/0002_Add_Two_Numbers/add_two_numbers.cpp
@@ -10,11 +10,16 @@ class Solution {
 public:
     // time complexity: O(max(m,n)), space complexity: O(max(m,n))
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        return iterative(l1, l2);
+        // return recursive(l1, l2);
+    }
+
+    ListNode* iterative(ListNode* l1, ListNode* l2){
         ListNode dummyHead(0);  // to avoid potential memory leak
         ListNode *curr = &dummyHead;
         int carry = 0;
 
-        while(l1 || l2){
+        while(l1 || l2 || carry){
             int x = l1 ? l1->val : 0;
             int y = l2 ? l2->val : 0;
             int sum = x + y + carry;
@@ -22,17 +27,43 @@ public:
 
             ListNode *node = new ListNode(sum % 10);
             curr->next = node;
-            curr = curr->next;
 
+            if(l1 || l2){
+                curr = curr->next;
+                l1 = l1 ? l1->next : NULL;
+                l2 = l2 ? l2->next : NULL;
+            }
+        }
+
+        return dummyHead.next;
+    }
+
+    ListNode* recursive(ListNode* l1, ListNode* l2){
+        return helper(l1, l2, 0);
+    }
+
+    ListNode* helper(ListNode* l1, ListNode* l2, int carry){
+        if(!l1 && !l2 && !carry)
+            return NULL;
+
+        ListNode dummyHead(0);  // to avoid potential memory leak
+        ListNode *curr = &dummyHead;
+        
+        int x = l1 ? l1->val : 0;
+        int y = l2 ? l2->val : 0;
+        int sum = x + y + carry;
+        carry = sum / 10;
+
+        ListNode *node = new ListNode(sum % 10);
+        curr->next = node;
+        
+        if(l1 || l2){
             if(l1) l1 = l1->next;
             if(l2) l2 = l2->next;
+            curr = curr->next;
+            curr->next = helper(l1, l2, carry);
         }
-
-        if(carry > 0){
-            ListNode *node = new ListNode(carry);
-            curr->next = node;
-        }
-
+        
         return dummyHead.next;
     }
 };
