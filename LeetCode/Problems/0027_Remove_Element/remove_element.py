7a0eb711603437c512fae6f87ebbaea2ad755e62
Joseph Chen <jocodoma@gmail.com>
2020-09-23 17:29:22 -0700

    [LeetCode] Code updates

--- LeetCode/Problems/0027_Remove_Element/remove_element.py
+++ LeetCode/Problems/0027_Remove_Element/remove_element.py
@@ -2,14 +2,14 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        return self.twoPointers(nums, val)
-        # return self.swapValues(nums, val)
+        return self.__twoPointers(nums, val)
+        # return self.__swapValues(nums, val)
 
     # time complexity: O(n), space complexity: O(1)
-    def twoPointers(self, nums: List[int], val: int) -> int:
+    def __twoPointers(self, nums: List[int], val: int) -> int:
         if len(nums) == 0:
             return 0
-          
+
         i = 0
         for n in nums:
             if n != val:
@@ -18,10 +18,10 @@ class Solution:
         return i
 
     # time complexity: O(n), space complexity: O(1)
-    def swapValues(self, nums: List[int], val: int) -> int:
+    def __swapValues(self, nums: List[int], val: int) -> int:
         if len(nums) == 0:
             return 0
-        
+
         l = 0
         r = len(nums) - 1
         while l <= r:
@@ -33,12 +33,19 @@ class Solution:
                 l += 1
         return l
 
+def printList(nums: List[int]) -> str:
+    return (', ').join(map(str, nums))
+
 nums = [3,2,2,3]
 val = 3
+print("Input: nums = [" + printList(nums) + "], val = " + str(val))
+
 size = Solution().removeElement(nums, val)
-print(nums[:size])
+print("Output: nums = [" + printList(nums[:size]) + "]\n")
 
 nums = [0,1,2,2,3,0,4,2]
 val = 2
+print("Input: nums = [" + printList(nums) + "], val = " + str(val))
+
 size = Solution().removeElement(nums, val)
-print(nums[:size])
+print("Output: nums = [" + printList(nums[:size]) + "]")
